52b0a682aafe464870b5ea7cc6e843a264f18d30
Nisham <moonblade168@gmail.com>
2016-02-27 21:18:56 +0530

    final State added

--- timedStates.py
+++ timedStates.py
@@ -29,7 +29,8 @@ def initTimedStates(states):
     states['in']={'in':states['in']}
 
     timedStates.append(states['in'])
-    for x in range(1,globalVariables.reverseEndIndex-globalVariables.reverseStartIndex):
+    runningTill=globalVariables.reverseEndIndex-globalVariables.reverseStartIndex
+    for x in range(1,runningTill):
         newState={}
         for state in globalVariables.stateList:
             newState[state]={}
@@ -43,4 +44,10 @@ def initTimedStates(states):
             for emission in globalVariables.emissionList:
                 timedStates[x][state]['emissions'][emission]=getTimedEmissionProbability(timedStates,state,emission,x)
 
+    finalState={'emissions':{},'probability':getTotalProabilityToState(timedStates[runningTill-1],'out'),'transitions':{}}
+    for emission in globalVariables.emissionList:
+        finalState['emissions'][emission]=0.0
+    for state in globalVariables.stateList:
+        finalState['transitions'][state]=0.0
+    timedStates.append({'out':finalState})
     return timedStates    
